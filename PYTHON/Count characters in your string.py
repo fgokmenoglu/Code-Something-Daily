29d720e03a8b77f0122ce7411920511f3e957b51
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-07-12 08:22:20 +0300

    Update Count characters in your string.py

--- PYTHON/Count characters in your string.py
+++ PYTHON/Count characters in your string.py
@@ -13,4 +13,17 @@ def count(s):
         # print(list_of_chars)
     
         return dict(zip(Counter(chars).keys(), Counter(chars).values()))
-      
+
+# ALTERNATIVE USING AGAIN! COUNTER
+"""
+from collections import Counter
+
+def count(string):
+    return Counter(string)
+"""
+
+"""
+Counter is an unordered collection where elements are stored as Dict keys and their count as dict value.
+We can create an empty Counter or start with some initial values too.
+We can also use any Iterable as argument for creating Counter object. So string literal and List can be used too for creating Counter object.
+"""

4329063bbd6164d189ac50f30f53d95b14e03339
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-07-21 08:52:04 +0300

    Create Do I get a bonus?.js

---
+++ JAVASCRIPT/Do I get a bonus?.js
@@ -0,0 +1,29 @@
+/*
+It's bonus time in the big city! The fatcats are rubbing their paws in anticipation... but who is going to make the most money?
+
+Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean.
+
+If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
+
+Return the total figure the individual will receive as a string prefixed with "£" (= "\u00A3", JS, Go, Java, Scala, and Julia), "$" (C#, C++, Ruby, Clojure, Elixir, PHP, Python, Haskell, and Lua) or "¥" (Rust).
+*/
+function bonusTime(salary, bonus) {
+  return "\u00A3" + (bonus ? salary * 10 : salary);
+}
+
+/*
+const a = 5;
+const b = 10;
+console.log("Fifteen is " + (a + b) + " and\nnot " + (2 * a + b) + ".");
+// "Fifteen is 15 and
+// not 20."
+
+With template literals, you can avoid the concatenation operator — and improve the readability of your code — by using placeholders of the form ${expression} to perform substitutions for embedded expressions:
+
+const a = 5;
+const b = 10;
+console.log(`Fifteen is ${a + b} and
+not ${2 * a + b}.`);
+// "Fifteen is 15 and
+// not 20."
+*/

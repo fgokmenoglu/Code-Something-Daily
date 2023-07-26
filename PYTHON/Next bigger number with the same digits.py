18a1f0e394ead687710bcd25aeed65e6bc249a71
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-07-26 09:34:07 +0300

    Update Next bigger number with the same digits.py

--- PYTHON/Next bigger number with the same digits.py
+++ PYTHON/Next bigger number with the same digits.py
@@ -10,3 +10,37 @@ If the digits can't be rearranged to form a bigger number, return -1 (or nil in 
 111 ==> -1
 531 ==> -1
 """
+# HAVING A TIMEOUT PROBLEM WITH THIS SOLUTION (TIMEOUT SET TO 12000 MS)
+import itertools
+
+def next_bigger(n):
+    possible_numbers = set()
+    
+    digits = [int(i) for i in list(str(n))]
+    # print(digits)
+    
+    for i in range(1, len(digits) + 1):
+        combinations = list(itertools.permutations(digits, i))
+        # print(combinations)
+        
+        for j in range(len(combinations)):
+            temp = 0
+            
+            for k in range(len(combinations[j])):     
+                temp += combinations[j][k] * (10 ** k)
+                
+            possible_numbers.add(temp)
+    
+    possible_numbers = list(possible_numbers)
+    possible_numbers.sort()
+    # print(possible_numbers)
+    
+    for number in possible_numbers:
+        # print(number)
+        index = possible_numbers.index(n)
+        # print(index)
+        
+        if index == (len(possible_numbers) - 1):
+            return (-1,)[0]
+        else:
+            return possible_numbers[index + 1]

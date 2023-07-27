f32766d9d04002079ecea1941a162983f24ec3d3
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-07-27 14:46:26 +0300

    Update Remove exclamation marks.js

--- JAVASCRIPT/Remove exclamation marks.js
+++ JAVASCRIPT/Remove exclamation marks.js
@@ -4,3 +4,6 @@ Write function RemoveExclamationMarks which removes all exclamation marks from a
 function removeExclamationMarks(s) {
   return s.replaceAll('!', '');
 }
+
+// ALTERNATIVE USING REGEX
+//  return s.replace(/!/g, '');

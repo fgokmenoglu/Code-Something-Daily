ad2d398b924c1223a6137f1ffc05de5c352f1d70
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-07-28 11:46:30 +0300

    Update Divide and Conquer.js

--- JAVASCRIPT/Divide and Conquer.js
+++ JAVASCRIPT/Divide and Conquer.js
@@ -3,3 +3,19 @@ Given a mixed array of number and string representations of integers, add up the
 
 Return as a number.
 */
+function divCon(x){
+  var sum_of_string_integers = 0;
+  var sum_of_nonstring_integers = 0;
+  
+  for (var i = 0; i < x.length; i++) {
+    if (typeof x[i] == 'string')
+      sum_of_string_integers += Number(x[i]);
+    else
+      sum_of_nonstring_integers += x[i];
+  }
+  
+  return sum_of_nonstring_integers - sum_of_string_integers;
+}
+
+// ALTERNATIVE USING REDUCE
+//  return x.reduce((acc, cur) => typeof cur === 'number' ? acc + cur : acc - Number(cur), 0)

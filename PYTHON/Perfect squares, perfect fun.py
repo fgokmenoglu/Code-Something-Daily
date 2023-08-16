936aca69767524476475e128ff96ea59b32b046a
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-08-16 17:22:10 +0300

    Update Perfect squares, perfect fun.py

--- PYTHON/Perfect squares, perfect fun.py
+++ PYTHON/Perfect squares, perfect fun.py
@@ -18,3 +18,30 @@ Note: 4 digits so 2 squared (2x2 perfect square). 2 digits on each line.
 
 Note: 9 digits so 3 squared (3x3 perfect square). 3 digits on each line.
 """
+def square_it(digits):
+    temp = list(str(digits))
+    # print(temp)
+    num_of_digits = len(temp)
+    square_root = num_of_digits ** 0.5
+    square_root = int(square_root + 0.5)
+    
+    if square_root * square_root != num_of_digits:
+        return "Not a perfect square!"
+    
+    result = ""
+    
+    for i in range(0, num_of_digits, int(square_root)):
+        result += (''.join(temp[i:i + int(square_root)]) + '\n')                         
+    
+    return result[:len(result) - 1]
+
+# ALTERNATIVE
+"""
+def square_it(digits):
+    s = str(digits)
+    n = len(s)**0.5
+    if n != int(n):
+        return "Not a perfect square!"
+    n = int(n)
+    return "\n".join(s[i*n:i*n+n] for i in range(int(n)))
+"""

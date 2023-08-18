f98903549856457f1c843f276502a207f439485e
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-08-18 08:43:57 +0300

    Create Last digit of a large number.py

---
+++ PYTHON/Last digit of a large number.py
@@ -0,0 +1,37 @@
+"""
+Define a function that takes in two non-negative integers a and b and returns the last decimal digit of a^b. 
+Note that a and b may be very large!
+
+You may assume that the input will always be valid.
+
+Examples
+last_digit(4, 1)                # returns 4
+last_digit(4, 2)                # returns 6
+last_digit(9, 7)                # returns 9
+last_digit(10, 10 ** 10)        # returns 0
+last_digit(2 ** 200, 2 ** 300)  # returns 6
+"""
+last_digit_pattern = {
+    0 : [0],
+    1 : [1],
+    2 : [2, 4, 6, 8],
+    3 : [3, 9, 7, 1],
+    4 : [4, 6],
+    5 : [5],
+    6 : [6],
+    7 : [7, 9, 3, 1],
+    8 : [8, 4, 2, 6],
+    9 : [9, 1]
+}
+
+def last_digit(n1, n2):
+    if n2 == 0:
+        return 1
+    
+    remainder1 = n1 % 10
+    
+    if len(last_digit_pattern[remainder1]) == 1:
+        return last_digit_pattern[remainder1][0]
+    
+    remainder2 = n2 % len(last_digit_pattern[remainder1])
+    return last_digit_pattern[remainder1][remainder2 - 1]

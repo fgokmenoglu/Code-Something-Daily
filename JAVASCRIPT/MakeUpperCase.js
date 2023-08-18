0e6fdceb5b58b6cb1f30ffd5a7e328dbfc1d4eb9
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-08-18 08:10:08 +0300

    Create MakeUpperCase.js

---
+++ JAVASCRIPT/MakeUpperCase.js
@@ -0,0 +1,15 @@
+/*
+Write a function which converts the input string to uppercase.
+*/
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
+
+// ALTERNATIVE USING ARROW FUNCTIONS
+// const makeUpperCase = (str) => str.toUpperCase();
+
+/*
+Arrow functions are always unnamed. 
+If the arrow function needs to call itself, use a named function expression instead. 
+You can also assign the arrow function to a variable so it has a name.
+*/

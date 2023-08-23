3699c5e726e5a921970a1f7ef05697d67dff32ef
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-08-23 15:15:36 +0300

    Update Two to One.js

--- JAVASCRIPT/Two to One.js
+++ JAVASCRIPT/Two to One.js
@@ -9,3 +9,19 @@ longest(a, b) -> "abcdefklmopqwxy"
 a = "abcdefghijklmnopqrstuvwxyz"
 longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
 */
+function longest(s1, s2) {
+  // console.log(s1 + s2);
+  
+  function onlyUnique(value, index, array) {
+    return array.indexOf(value) === index;
+  }
+  
+  var unique_arr = (s1 + s2).split('').filter(onlyUnique);
+  unique_arr.sort();
+  var unique_str = unique_arr.join('');
+  
+  // console.log(unique_str);
+  return unique_str;
+}
+// ALTERNATIVE
+// const longest = (s1, s2) => [...new Set(s1+s2)].sort().join('')

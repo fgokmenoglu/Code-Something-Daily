629c85386ccca6530e2d39ab94f8b811744da79c
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-09-07 13:49:11 +0300

    Update Sort the odd.py

--- PYTHON/Sort the odd.py
+++ PYTHON/Sort the odd.py
@@ -7,3 +7,27 @@ Examples
 [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
 [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 """
+def sort_array(source_array):
+
+    odds = []
+    answer = []
+    
+    for i in source_array:
+        if i % 2 > 0:
+            odds.append(i)
+            answer.append("X")
+            
+        else:
+            answer.append(i)
+            
+    odds.sort()
+    
+    for i in odds:
+        x = answer.index("X")
+        answer[x] = i
+      
+    return answer
+
+# ALTERNATIVE
+# odds = sorted((x for x in arr if x % 2 != 0), reverse = True)
+# return [x if x % 2 == 0 else odds.pop() for x in arr]

9fb80b55c6cd94d9f4c36d5ffeb5c6943a98dfc3
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-09-13 07:35:22 +0300

    Update Printer Errors.js

--- JAVASCRIPT/Printer Errors.js
+++ JAVASCRIPT/Printer Errors.js
@@ -28,13 +28,12 @@ function printerError(s) {
 
 // ALTERNATIVE WITHOUT REGEX
 function printerError(s) {
-    // your code
     var count = 0;
-    for(var i = 0; i < s.length; i++) {
-      if (s[i] > "m") {
+  
+    for(var i = 0; i < s.length; i++)
+      if (s[i] > "m")
         count++;
-      }
-    }
+  
     return count + "/" + s.length;
 }
 

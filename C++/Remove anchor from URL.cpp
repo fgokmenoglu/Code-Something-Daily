c3f89f162ec80d7a7cf155c8856a8d587728cf22
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-09-15 09:31:14 +0300

    Update Remove anchor from URL.cpp

--- C++/Remove anchor from URL.cpp
+++ C++/Remove anchor from URL.cpp
@@ -5,3 +5,6 @@
  * "www.codewars.com#about" --> "www.codewars.com"
  * "www.codewars.com?page=1" -->"www.codewars.com?page=1"
  */
+std::string replaceAll(std::string str) {
+  return str.substr(0, str.find('#'));
+}

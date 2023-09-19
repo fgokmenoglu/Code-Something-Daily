a83a4b2870481e0976f7e9179a38957e9947ebe3
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-09-19 11:39:05 +0300

    Create Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence.cpp

---
+++ C++/Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence.cpp
@@ -0,0 +1,24 @@
+/*
+ * Description:
+ * Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+ * 
+ * Examples
+ * replace("Hi!") === "H!!"
+ * replace("!Hi! Hi!") === "!H!! H!!"
+ * replace("aeiou") === "!!!!!"
+ * replace("ABCDE") === "!BCD!"
+ */
+#include <string>
+
+using namespace std;
+
+string replace(const string &s)
+{
+  string vowels = "aeiouAEIOU";
+  string result = s;
+
+  while (result.find_first_of(vowels) != string::npos)
+    result[result.find_first_of(vowels)] = '!'; 
+  
+  return result;
+}

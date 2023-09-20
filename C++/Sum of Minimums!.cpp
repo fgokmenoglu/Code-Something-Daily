1074e58f248ff60ed3fd033f0423c409f886b32b
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-09-20 09:31:54 +0300

    Update Sum of Minimums!.cpp

--- C++/Sum of Minimums!.cpp
+++ C++/Sum of Minimums!.cpp
@@ -11,3 +11,31 @@
  * 
  * Note: You will always be given a non-empty list containing positive values.
  */
+#include <vector>
+
+int sum_of_minimums(const std::vector<std::vector<int>> &numbers)
+{
+  int rows = numbers.size();
+  // int cols = numbers[0].size();
+
+  int result = 0;
+  
+  for (int i = 0; i < rows; i++)
+  {
+    result += *(std::min_element(numbers[i].begin(), numbers[i].end()));
+  }
+  
+  return result;
+}
+
+// ALTERNATIVE
+#include <numeric>
+
+int sum_of_minimums(std::vector<std::vector<int>> &numbers) 
+{
+    return std::accumulate(numbers.begin(), numbers.end(), 0, [](auto r, auto e){ return r + *std::min_element(e.begin(), e.end()); });
+}
+
+// [] is called the lambda introducer which denotes the start of the lambda expression
+// () is called the parameter list which is similar to the () operator of a normal function
+// By default, lambda functions cannot access variables of the enclosing function. In order to access those variables, we use the capture clause [].

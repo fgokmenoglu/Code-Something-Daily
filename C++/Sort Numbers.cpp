9b958ee45b8aae58542a99342a32c1fddb2a5687
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-09-22 07:40:45 +0300

    Create Sort Numbers.cpp

---
+++ C++/Sort Numbers.cpp
@@ -0,0 +1,16 @@
+/*
+ * Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+ * 
+ * For example:
+ * 
+ * sortNumbers({1, 2, 10, 50, 5}) // sholud return {1, 2, 5, 10, 50}
+ * sortNumbers({}) // should return {}
+ */
+#include <vector>
+
+std::vector<int> solution(std::vector<int> nums) 
+{
+  std::sort(nums.begin(), nums.end());
+  
+  return nums;
+}

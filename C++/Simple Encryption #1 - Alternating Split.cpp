577930afd558eebe5a584a799d6645d0f7006b2c
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-09-28 08:37:48 +0300

    Update Simple Encryption #1 - Alternating Split.cpp

--- C++/Simple Encryption #1 - Alternating Split.cpp
+++ C++/Simple Encryption #1 - Alternating Split.cpp
@@ -13,3 +13,34 @@
  * Together with the encryption function, you should also implement a decryption function which reverses the process.
  * If the string S is an empty value or the integer N is not positive, return the first argument without changes.
  */
+std::string encrypt(std::string text, int n)
+{
+    if (text.empty() || n <= 0)
+      return text;
+  
+    std::string temp = "";
+    
+    // std::cout << text << std::endl;
+  
+    for (int i = 1; i <= n; i++)
+    {      
+      for (unsigned long i = 0; i < text.size(); i++)
+        if (i % 2 == 1)
+          temp.push_back(text[i]); 
+  
+      for (unsigned long i = 0; i < text.size(); i++)
+        if (i % 2 == 0)
+          temp.push_back(text[i]);
+      
+      text = temp;
+      // std::cout << text << std::endl;
+      temp = "";
+    }
+  
+    return text;
+}
+
+std::string decrypt(std::string encryptedText, int n)
+{
+    return encryptedText;
+}

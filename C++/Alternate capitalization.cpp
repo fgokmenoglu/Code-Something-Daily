98dcc82e8f1892d66637c463f29a5a6a0ee6eb80
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-09-28 13:31:03 +0300

    Update Alternate capitalization.cpp

--- C++/Alternate capitalization.cpp
+++ C++/Alternate capitalization.cpp
@@ -5,3 +5,17 @@
  * 
  * The input will be a lowercase string with no spaces.
  */
+std::pair<std::string, std::string> capitalize(const std::string &s) {
+  std::string first, second;
+  
+  for (size_t i = 0; i < s.length(); i++)
+    if(i % 2 == 0) {
+      first.push_back(toupper(s[i]));
+      second.push_back(s[i]);
+    } else {
+      first.push_back(s[i]);
+      second.push_back(toupper(s[i]));
+    }   
+  
+  return {first, second};
+}

08202d92d2d6c920f38c54b32c3fa92965185953
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-09-29 08:19:29 +0300

    Create Small enough? - Beginner.cpp

---
+++ C++/Small enough? - Beginner.cpp
@@ -0,0 +1,14 @@
+/*
+ * You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+ * 
+ * You can assume all values in the array are numbers.
+ */
+#include <vector>
+
+bool small_enough(std::vector<int> arr, int limit) {
+  for (int num : arr)
+    if (num > limit)
+      return false;
+  
+  return true;
+}

389e52a2ab31e1ed64b4b05baa1116fad9f2f76a
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-10-03 08:03:04 +0300

    Update Fix string case.cpp

--- C++/Fix string case.cpp
+++ C++/Fix string case.cpp
@@ -9,3 +9,38 @@
  * solve("CODe") = "CODE". Uppercase characters > lowecase. Change only the "e" to uppercase.
  * solve("coDE") = "code". Upper == lowercase. Change all to lowercase.
  */
+#include <string>
+#include <algorithm>
+
+std::string solve(const std::string& str) {
+  std::string result = str;
+  int lowerCount = 0, upperCount = 0;
+  
+  for (auto ch(result.begin()); ch != result.end(); ch++)
+    if (*ch >= 65 && *ch <= 90)
+      upperCount++;
+    else if (*ch >= 97 && *ch <= 122)
+      lowerCount++;
+  
+  if (upperCount > lowerCount)
+    std::transform(result.begin(), result.end(), result.begin(), ::toupper);  
+  else
+    std::transform(result.begin(), result.end(), result.begin(), ::tolower);
+  
+  return result;
+}
+
+// ALTERNATIVE
+#include <string>
+#include <algorithm>
+
+std::string solve(const std::string& str) {
+  std::string result = str;
+  bool bLower = result.size() <= 2 * std::count_if(result.begin(), result.end(), ::islower);
+  
+  std::transform(result.begin(), result.end(), result.begin(), [&] (auto ch) {
+    return bLower ? std::tolower(ch) : std::toupper(ch);
+  });
+  
+  return result;
+}

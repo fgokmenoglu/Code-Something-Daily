e608b1786a7a60dce684fbb7955d775ef87ec96b
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-10-06 07:34:03 +0300

    Update Tidy Number (Special Numbers Series #9).cpp

--- C++/Tidy Number (Special Numbers Series #9).cpp
+++ C++/Tidy Number (Special Numbers Series #9).cpp
@@ -18,3 +18,34 @@
  * Explanation:
  * The Number's Digits { 3, 2} are not in non-Decreasing Order (i.e) 3 > 2 .
  */
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std; 
+
+bool tidyNumber (int number) {
+  vector<int> digits = {};
+  
+  while (number > 0) {
+    digits.push_back(number % 10);
+    number /= 10;
+  }
+  
+  reverse(digits.begin(), digits.end());
+  
+  return is_sorted(digits.begin(), digits.end());
+}
+
+// ALTERNATIVE
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+bool tidyNumber (int number) {
+  string num = to_string(number);
+  sort(num.begin(), num.end());
+  
+  return num == to_string(number) ? true : false;
+}

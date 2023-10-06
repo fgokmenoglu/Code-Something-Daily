008342f8fc94fcc232bdad2251e6f58feb707b63
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-10-06 07:22:48 +0300

    Create  Tidy Number (Special Numbers Series #9).cpp

---
+++ C++/Tidy Number (Special Numbers Series #9).cpp
@@ -0,0 +1,20 @@
+/*
+ * Definition
+ * A Tidy number is a number whose digits are in non-decreasing order.
+ * 
+ * Task
+ * Given a number, Find if it is Tidy or not.
+ * 
+ * Notes
+ * Number passed is always Positive.
+ * Return the result as a Boolean
+ * 
+ * Input >> Output Examples
+ * tidyNumber (12) ==> return (true)
+ * Explanation:
+ * The number's digits { 1 , 2 } are in non-Decreasing Order (i.e) 1 <= 2 .
+ * 
+ * tidyNumber (32) ==> return (false)
+ * Explanation:
+ * The Number's Digits { 3, 2} are not in non-Decreasing Order (i.e) 3 > 2 .
+ */

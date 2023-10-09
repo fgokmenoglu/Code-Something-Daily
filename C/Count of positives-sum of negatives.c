febdaf993f006a17b94044ab20a17bee7923fe76
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-10-09 10:07:19 +0300

    Update Count of positives-sum of negatives.c

--- C/Count of positives-sum of negatives.c
+++ C/Count of positives-sum of negatives.c
@@ -8,3 +8,34 @@
  * Example
  * For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
  */
+#include <stddef.h>
+
+void count_positives_sum_negatives(int *values, size_t count, int *positivesCount, int *negativesSum) 
+{
+  *positivesCount = 0;
+  *negativesSum = 0;
+  
+  for (size_t i = 0; i < count; i++) {
+    if (*values > 0)
+      ++(*positivesCount);
+    else if(*values < 0)
+      *negativesSum += *values;
+  
+    ++values;
+  }
+} 
+
+// ALTERNATIVE
+#include <stddef.h>
+
+void count_positives_sum_negatives(int *values, size_t count, int *positivesCount, int *negativesSum) 
+{
+  *positivesCount = 0;
+  *negativesSum = 0;
+  
+  for (size_t i = 0; i < count; i++)
+    if (values[i] > 0)
+      ++(*positivesCount);
+    else if(values[i] < 0)
+      *negativesSum += values[i];
+} 

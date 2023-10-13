6279324671753f80cb16718b370f9e9af3ad2b25
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-10-13 08:21:07 +0300

    Update Number of Decimal Digits.cpp

--- C++/Number of Decimal Digits.cpp
+++ C++/Number of Decimal Digits.cpp
@@ -3,3 +3,28 @@
  * For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
  * All inputs will be valid.
  */
+#include <stdint.h>
+
+int digits(uint64_t n) {
+  int numDigits = 0;
+  
+  if (n == 0)
+    return 1;
+  
+  while(n > 0) {
+    ++numDigits;
+    n /= 10;
+  }
+  
+  return numDigits;
+}
+
+// ALTERNATIVE
+#include <stdint.h>
+#include <string>
+
+using namespace std;
+
+int digits(uint64_t n) {
+  return to_string(n).length();
+}

7ce668a813880c1f835dad0507d4fc8052572167
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-10-20 11:36:24 +0300

    Update Consonant value.cpp

--- C++/Consonant value.cpp
+++ C++/Consonant value.cpp
@@ -11,3 +11,33 @@
  * For the word "strength", solve("strength") = 57
  * -- The consonant substrings are: "str" and "ngth" with values "str" = 19 + 20 + 18 = 57 and "ngth" = 14 + 7 + 20 + 8 = 49. The highest is 57.
  */
+#include <string>
+
+int consonantValue(char c) {
+    if (c >= 'a' && c <= 'z') {
+        return c - 'a' + 1;
+    }
+    return 0;
+}
+
+int solve(const std::string& s) {
+    int maxConsonantValue = 0;
+    int currentConsonantValue = 0;
+
+    for (char c : s) {
+        if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u') {
+            currentConsonantValue += consonantValue(c);
+        } else {
+            if (currentConsonantValue > maxConsonantValue) {
+                maxConsonantValue = currentConsonantValue;
+            }
+            currentConsonantValue = 0;
+        }
+    }
+
+    if (currentConsonantValue > maxConsonantValue) {
+        maxConsonantValue = currentConsonantValue;
+    }
+
+    return maxConsonantValue;
+}

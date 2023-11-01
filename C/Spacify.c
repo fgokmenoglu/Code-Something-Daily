782ce2060481695f27e4a9fe2c1a1d9e1c0b2586
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-11-01 09:15:43 +0300

    Update Spacify.c

--- C/Spacify.c
+++ C/Spacify.c
@@ -4,7 +4,18 @@
  * spacify("hello world") // returns "h e l l o   w o r l d"
  */
 #include <stdlib.h>
+#include <string.h>
 
-char *spacify(const char *str_in) {
-
+char * spacify(const char *str_in) {
+  char *output = (char *) malloc(2 * strlen(str_in) * sizeof(char));
+  int index = 0;
+  
+  for (int i = 0; str_in[i] != '\0'; i++) {
+    output[index++] = str_in[i];
+    output[index++] = ' ';
+  }
+  
+  output[index - 1] = '\0';
+  
+  return output;    
 }

be156ea84def19b2dd2f16f4c4c6814d543889fc
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-11-09 08:54:26 +0300

    Update Alphabet symmetry.cpp

--- C++/Alphabet symmetry.cpp
+++ C++/Alphabet symmetry.cpp
@@ -8,3 +8,44 @@
  * 
  * Input will consist of alphabet characters, both uppercase and lowercase. No spaces.
  */
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+std::vector<int> solve(const std::vector<std::string>& arr) {  
+  std::vector<int> output = {};
+  std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
+  int temp1 = 0;
+  
+  for (size_t i = 0; i < arr.size(); i++) {
+    std::string temp2 = arr[i];
+    std::transform(temp2.begin(), temp2.end(), temp2.begin(), [](unsigned char c) { return std::tolower(c); });
+    
+    for (size_t j = 0; j < temp2.length(); j++) {
+      if (temp2[j] == alphabet[j])
+        temp1 += 1;
+    }
+    
+    output.push_back(temp1);
+    temp1 = 0;
+  }
+  
+  return output;
+}
+
+// ALTERNATIVE
+#include <vector>
+#include <string>
+#include <cctype>
+
+std::vector<int> solve(std::vector<std::string> v) {
+  std::vector<int> result(v.size());
+  
+  for (int i = 0; i < v.size(); i++)
+    for (int j = 0; j < v[i].length(); j++) 
+      if (toupper(v[i][j]) - 65 == j)
+        result[i]++;
+  
+  return result;
+}

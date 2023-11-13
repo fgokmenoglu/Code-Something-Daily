29fc4375c7606434eaf5a6b035409e476caa7c3f
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-11-13 10:28:13 +0300

    Update Filter Long Words.cpp

--- C++/Filter Long Words.cpp
+++ C++/Filter Long Words.cpp
@@ -8,7 +8,33 @@
  */
 #include <vector>
 #include <string>
+#include <sstream>
 
 std::vector<std::string> filter_long_words(const std::string& sentence, int n) {
-  // your code here
+  std::stringstream ss(sentence);
+  std::vector<std::string> tokens = {};
+  std::string token = "";
+  std::vector<std::string> output = {};
+  
+  while (getline(ss, token, ' '))
+    tokens.push_back(token);
+  
+  for (size_t i = 0; i < tokens.size(); i++)
+    if (tokens[i].length() > n)
+      output.push_back(tokens[i]);
+      
+  return output;
+}
+
+// ALTERNATIVE
+#include <vector>
+#include <string>
+#include <istream>
+
+std::vector<std::string> filter_long_words(const std::string& sentence, int n) {
+  std::vector<std::string> r;
+  std::istringstream ss(sentence);
+  std::string s;
+  while (ss >> s) if (s.size() > n) r.push_back(s);
+  return r;
 }

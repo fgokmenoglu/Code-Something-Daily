11bf69b0a66e83563c1d5412e65ac6a808dde44a
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-11-15 08:06:03 +0300

    Update Convert an array of strings to array of numbers.cpp

--- C++/Convert an array of strings to array of numbers.cpp
+++ C++/Convert an array of strings to array of numbers.cpp
@@ -13,5 +13,36 @@
 #include <string>
 
 std::vector<float> to_float_array(const std::vector<std::string>& arr) {
-  // your code here
+  std::vector<float> output = {};
+  
+  for (size_t i = 0; i < arr.size(); i++)
+    output.push_back(stof(arr[i]));
+  
+  return output;
+}
+
+// ALTERNATIVE 1
+#include <vector>
+#include <string>
+
+std::vector<float> to_float_array(const std::vector<std::string>& arr) {
+  std::vector<float> ans;
+  
+  for(auto s : arr)
+    ans.push_back(stof(s));
+  
+  return ans;
+}
+
+// ALTERNATIVE 2
+#include <vector>
+#include <string>
+#include <algorithm>
+
+std::vector<float> to_float_array(const std::vector<std::string>& arr) 
+{
+  std::vector<float> numbers(arr.size());
+  std::transform(arr.cbegin(), arr.cend(), numbers.begin(), [](const auto& s) { return std::stof(s); });
+  
+  return numbers;
 }

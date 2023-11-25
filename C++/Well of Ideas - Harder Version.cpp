063fc8f550f0afcca06ee624a6f5ac778e5b6677
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-11-25 15:04:11 +0300

    Update Well of Ideas - Harder Version.cpp

--- C++/Well of Ideas - Harder Version.cpp
+++ C++/Well of Ideas - Harder Version.cpp
@@ -35,3 +35,16 @@ std::string well(const std::vector<std::vector<std::string>>& arr) {
 }
 
 // ALTERNATIVE
+#include <string>
+#include <vector>
+#include <cctype>
+
+
+std::string well(const std::vector< std::vector< std::string >>& a) {
+  unsigned c{};
+  
+  for (const auto& v : a)
+    for (const auto& s : v)
+      c += tolower(s.front()) == 'g';
+  return c > 2 ? "I smell a series!" : c ? "Publish!" : "Fail!";
+}

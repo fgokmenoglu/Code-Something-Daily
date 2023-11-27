ab9afe7e7d011fc21828536956749a4eaac70887
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-11-27 08:12:08 +0300

    Update Max-min arrays.cpp

--- C++/Max-min arrays.cpp
+++ C++/Max-min arrays.cpp
@@ -7,7 +7,42 @@
  * The first max is 15 and the first min is 7. The second max is 12 and the second min is 10 and so on.
  */
 #include <vector>
+#include <algorithm>
 
-std::vector<int> solve(std::vector<int>v) {
-    //..
+std::vector<int> solve(std::vector<int> v) {
+    std::vector<int> output = {};
+    sort(v.begin(), v.end());
+    
+    size_t i = 0;
+    size_t j = v.size() - 1;
+
+    while (i <= j) {
+        if (i != j) {
+            output.push_back(v[j--]); // Add the max value
+            output.push_back(v[i++]); // Add the min value
+        } else {
+            // If there's an odd number of elements, add the middle element
+            output.push_back(v[i]);
+            break;
+        }
+    }
+    
+    return output;
+}
+
+// ALTERNATIVE
+#include <algorithm>
+
+using std::max_element;
+using std::min_element;
+using std::swap;
+
+std::vector<int> solve(std::vector<int> v) {
+  for (auto it {v.begin()}; it != v.end(); ++it) {
+    swap (*it, *max_element(it, v.end()));
+        
+    if (++it != v.end()) swap (*it, *min_element(it, v.end()));
+  }
+    
+  return v;
 }

7af2807b3a7c053991853b7ef62a701515b2093d
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-12-05 07:21:57 +0300

    Create Product Array (Array Series #5).cpp

---
+++ C++/Product Array (Array Series #5).cpp
@@ -0,0 +1,17 @@
+/*
+ * Task
+ * Given an array/list [] of integers, construct a product array of same size such that prod[i] is equal to the product of all the elements of arr[] except arr[i].
+ * 
+ * Notes
+ * Array/list size is at least 2.
+ * Array/list's numbers will be only positives.
+ * Repetition of numbers in the array/list could occur.
+ */
+#include <vector>
+
+using namespace std; 
+
+vector <int> productArray (vector <int> numbers) {
+  // Your Code is Here ... Enjoy !!!
+  return 1 ;
+}

d3527618e366265cf14a9af4f139c67120b33e22
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-12-09 09:00:50 +0300

    Update Even numbers in an array.cpp

--- C++/Even numbers in an array.cpp
+++ C++/Even numbers in an array.cpp
@@ -8,3 +8,41 @@
  * ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
  * ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
  */
+#include <vector>
+
+std::vector<int> evenNumbers(std::vector<int> arr, int n) {
+  std::vector<int> output = {};
+  std::vector<int> temp = arr;
+  int count = 0;
+  
+  reverse(temp.begin(), temp.end());
+  
+  for (size_t i = 0; i < temp.size(); ++i)
+    if (temp[i] % 2 == 0 && count < n) {
+      output.push_back(temp[i]);
+      ++count;
+    }
+  
+  reverse(output.begin(), output.end());
+  
+  return output;
+}
+
+// ALTERNATIVE
+#include <vector>
+
+std::vector<int> evenNumbers(std::vector<int> arr, int n) {
+  std::vector<int> earr(n);
+  auto i = arr.rbegin();
+  
+  while ( n > 0 && i != arr.rend() ) {
+    if ( *i % 2 == 0 ) {
+      earr[n-1] = *i;
+      n -= 1;
+    }
+    
+    i++;
+  }
+  
+  return earr;
+}

66f5572e9bc44fae727f50cf4cb0cdcbc1478aff
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-12-14 09:18:39 +0300

    Update Simple string characters.cpp

--- C++/Simple string characters.cpp
+++ C++/Simple string characters.cpp
@@ -5,7 +5,43 @@
  * --the order is: uppercase letters, lowercase, numbers and special characters.
  */
 #include <vector>
+#include <string>
+#include <algorithm>
+#include <cctype>
 
 std::vector<int> solve(std::string s) {
-    //..
+  std::vector<int> output = {};
+  int countUpper = 0, countLower = 0, countNumber = 0, countSpecial = 0;
+  
+  countUpper = std::count_if(s.begin(), s.end(), [](const char ch) { return std::isupper(ch); });
+  countLower = std::count_if(s.begin(), s.end(), [](const char ch) { return std::islower(ch); });
+  countNumber = std::count_if(s.begin(), s.end(), [](const char ch) { return std::isdigit(ch); });
+  countSpecial = std::count_if(s.begin(), s.end(), [](const char ch) { return !std::isalnum(ch); });
+  
+  output = {countUpper, countLower, countNumber, countSpecial};
+  return output;
+}
+
+// ALTERNATIVE
+#include <string>
+#include <vector>
+#include <cctype>
+
+std::vector<int> solve(const std::string &s) {
+    int countUpper = 0, countLower = 0, countDigit = 0, countSpecial = 0;
+
+    for (char ch : s) {
+        if (std::isupper(ch)) {
+            ++countUpper;
+        } else if (std::islower(ch)) {
+            ++countLower;
+        } else if (std::isdigit(ch)) {
+            ++countDigit;
+        } else {
+            ++countSpecial;
+        }
+    }
+
+    std::vector<int> result = {countUpper, countLower, countDigit, countSpecial};
+    return result;
 }

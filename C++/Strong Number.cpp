166ff2f60cdbd8a3fa33390ef4d0d70d685e5108
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-12-19 07:28:34 +0300

    Create Strong Number.cpp

---
+++ C++/Strong Number.cpp
@@ -0,0 +1,13 @@
+/*
+ * Definition
+ * Strong number is the number that the sum of the factorial of its digits is equal to number itself.
+ * 
+ * For example, 145 is strong, since 1! + 4! + 5! = 1 + 24 + 120 = 145.
+ * 
+ * Task
+ * Given a number, Find if it is Strong or not and return either "STRONG!!!!" or "Not Strong !!".
+ * 
+ * Notes
+ * Number passed is always Positive.
+ * Return the result as String
+ */

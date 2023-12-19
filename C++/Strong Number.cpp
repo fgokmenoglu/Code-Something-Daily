6e38c699f7fd503f23eecbb3c08a7c467675feed
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-12-19 07:40:43 +0300

    Update Strong Number.cpp

--- C++/Strong Number.cpp
+++ C++/Strong Number.cpp
@@ -11,3 +11,43 @@
  * Number passed is always Positive.
  * Return the result as String
  */
+#include <string>
+
+using namespace std;
+
+int factorial(int number) {
+  if (number == 1 || number == 0)
+    return 1;
+  else
+    return number * factorial(number - 1);
+}
+
+string strong_num(int number) {
+  cout << number << endl;
+  int temp = number, sum = 0;
+  
+  while (temp > 0) {
+    sum += factorial(temp % 10);
+    temp /= 10;
+  }
+  
+  return number == sum ? "STRONG!!!!" : "Not Strong !!"; 
+}
+
+// ALTERNATIVE
+#include <string>
+
+using namespace std; 
+
+string strong_num(int number) {
+    int fac[10] = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
+    int sum = 0;
+    int temp = number;
+  
+    while(temp > 0) {
+      sum += fac[temp % 10];
+      temp /= 10;
+    }
+  
+    return sum == number ? "STRONG!!!!" : "Not Strong !!"; 
+}

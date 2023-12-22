576303dc4fa429503301b66cf5414dbeef1002f1
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-12-22 09:47:26 +0300

    Update Number climber.cpp

--- C++/Number climber.cpp
+++ C++/Number climber.cpp
@@ -9,3 +9,47 @@
  * Write a function that returns this sequence given a number N. 
  * Try generating the elements of the resulting list in ascending order, i.e., without resorting to a list reversal or prependig the elements to a list.
  */
+#include <vector>
+
+std::vector<int> climb(int n) {
+    std::vector<int> sequence;
+    
+    // Start from n and work backwards
+    while (n > 1) {
+        sequence.push_back(n);
+        // If n is odd, subtract 1 and divide by 2
+        // If n is even, just divide by 2
+        n = (n % 2) ? (n - 1) / 2 : n / 2;
+    }
+
+    // Add the starting element
+    sequence.push_back(1);
+
+    // Reverse the sequence to get it in ascending order
+    std::reverse(sequence.begin(), sequence.end());
+    
+    return sequence;
+}
+
+// ALTERNATIVE
+#include <vector>
+#include <cmath>
+
+std::vector<int> climb(int n) {
+  int x, msb;
+  int idx = 0;
+  std::vector<int> l;
+
+  // Get index of most significant bit
+  for (x = n, msb = 0; x != 1; x >>= 1, msb++);
+
+  l.push_back(1); // Add 1
+  
+  for (int i = msb - 1; i >= 0; i--) {
+    idx = (static_cast<int>(std::pow(2, i)) & n) > 0 ? 1 : 0;
+    x = 2 * x + idx;
+    l.push_back(x); // Add next number
+  }
+  
+  return l;
+}

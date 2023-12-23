bea33e1a805ef76420f489cf73cf187e612dd286
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-12-23 13:36:47 +0300

    Create Maximum Triplet Sum.cpp

---
+++ C++/Maximum Triplet Sum.cpp
@@ -0,0 +1,14 @@
+/*
+ * Task
+ * Given an array/list [] of n integers , find maximum triplet sum in the array Without duplications .
+ * 
+ * Notes :
+ * Array/list size is at least 3.
+ * Array/list numbers could be a mixture of positives , negatives and zeros .
+ * Repetition of numbers in the array/list could occur.
+ * Duplications are not included when summing , (i.e) the numbers added only once.
+ * 
+ * Input >> Output Examples
+ * 1- maxTriSum ({3,2,6,8,2,3}) ==> return (17)
+ * Explanation: As the triplet that maximize the sum {6,8,3} in order , their sum is 17.
+ */

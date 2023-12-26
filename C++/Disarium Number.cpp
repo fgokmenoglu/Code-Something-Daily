abb099ff6095afd0499026fc937e8b6443850c20
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-12-26 07:51:31 +0300

    Create Disarium Number.cpp

---
+++ C++/Disarium Number.cpp
@@ -0,0 +1,11 @@
+/*
+ * Definition
+ * Disarium number is the number that the sum of its digits powered with their respective positions is equal to the number itself.
+ * 
+ * Task
+ * Given a number, Find if it is Disarium or not.
+ * 
+ * Notes
+ * Number passed is always positive.
+ * Return the result as string.
+ */

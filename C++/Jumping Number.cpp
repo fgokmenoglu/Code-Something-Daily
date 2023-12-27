c729c55f1d3a1d4800adbc485ad524aa7f92a236
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-12-27 07:24:28 +0300

    Create Jumping Number.cpp

---
+++ C++/Jumping Number.cpp
@@ -0,0 +1,20 @@
+/*
+ * Definition
+ * Jumping number is the number that All adjacent digits in it differ by 1.
+ * 
+ * Task
+ * Given a number, Find if it is Jumping or not .
+ * 
+ * Notes
+ * Number passed is always Positive .
+ * Return the result as String .
+ * The difference between ‘9’ and ‘0’ is not considered as 1 .
+ * All single digit numbers are considered as Jumping numbers.
+ */
+#include <string>
+
+using namespace std; 
+
+string jumpingNumber (int number) {
+
+}

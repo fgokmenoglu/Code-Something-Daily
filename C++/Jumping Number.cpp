e20489f6b69275f16d5e18a8b26aeee79e3051fc
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-12-27 07:45:15 +0300

    Update Jumping Number.cpp

--- C++/Jumping Number.cpp
+++ C++/Jumping Number.cpp
@@ -12,9 +12,45 @@
  * All single digit numbers are considered as Jumping numbers.
  */
 #include <string>
+#include <cstdlib>
+
+using namespace std; 
+
+string jumpingNumber(int number) {
+  string numStr = to_string(number);
+  
+  if(numStr.length() == 1)
+    return "Jumping!!";
+  
+  for(size_t i = 0; i < numStr.length() - 1; ++i)
+    if (abs((numStr[i + 1] - '0') - (numStr[i] - '0')) != 1)
+      return "Not!!";
+  
+  return "Jumping!!";
+}
+
+// ALTERNATIVE
+#include <string>
+
+using namespace std; 
+
+string jumpingNumber(int number) {
+  if (number < 10) return "Jumping!!";
+  int diff = number % 10 - number / 10 % 10;
+  return diff == 1 || diff == -1 ? jumpingNumber(number / 10) : "Not!!";
+}
+
+// ALTERNATIVE 2
+#include <string>
 
 using namespace std; 
 
 string jumpingNumber (int number) {
-
+  string s = to_string(number);
+  
+  for (size_t i = 1; i < s.size(); ++i)
+    if (abs(s[i] - s[i - 1]) != 1)
+      return "Not!!";
+  
+  return "Jumping!!";
 }

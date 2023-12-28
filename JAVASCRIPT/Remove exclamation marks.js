9f7ac6dd44f33df59de978fa3424e51511144328
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-12-28 13:43:11 +0300

    Delete JAVASCRIPT directory

--- JAVASCRIPT/Remove exclamation marks.js
+++
@@ -1,9 +0,0 @@
-/*
-Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
-*/
-function removeExclamationMarks(s) {
-  return s.replaceAll('!', '');
-}
-
-// ALTERNATIVE USING REGEX
-//  return s.replace(/!/g, '');

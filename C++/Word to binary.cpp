70d8b2c64ab4fca6254e2fd35d84b7bc95540c22
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-12-30 08:42:26 +0300

    Update Word to binary.cpp

--- C++/Word to binary.cpp
+++ C++/Word to binary.cpp
@@ -5,7 +5,33 @@
  */
 #include <vector>
 #include <string>
+#include <bitset>
 
 std::vector<std::string> word_to_bin(std::string word) {
-  // your code here
+  std::vector<std::string> output = {};
+  std::string binaryStr = "";
+  
+  for (size_t i = 0; i < word.length(); ++i) {
+    binaryStr = std::bitset<8>(word[i]).to_string();
+    output.push_back(binaryStr);
+  }
+  
+  return output;
+}
+
+// ALTERNATIVE
+#include <vector>
+#include <string>
+#include <bitset>
+
+constexpr size_t ascii_bits = 8;
+
+std::vector<std::string> word_to_bin(const std::string &word) {
+  std::vector<std::string> result;
+  result.reserve(word.size());
+    
+  for (const char &ch : word) 
+    result.push_back(std::bitset<ascii_bits>(ch).to_string());
+  
+  return result;
 }

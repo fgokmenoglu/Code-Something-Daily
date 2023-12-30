87c96fbe5bb7049c3f8b0e5c66b37bf10445dd0a
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-12-30 08:32:38 +0300

    Create Word to binary.cpp

---
+++ C++/Word to binary.cpp
@@ -0,0 +1,11 @@
+/*
+ * Write a function that takes a string and returns an array containing binary numbers equivalent to the ASCII codes of the characters of the string. 
+ * The binary strings should be eight digits long.
+ * Example: 'man' should return [ '01101101', '01100001', '01101110' ]
+ */
+#include <vector>
+#include <string>
+
+std::vector<std::string> word_to_bin(std::string word) {
+  // your code here
+}

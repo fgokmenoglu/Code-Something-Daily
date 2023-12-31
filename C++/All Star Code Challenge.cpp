4a385e341c39a002fa64c6a7ce2fe4441845ed2d
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-12-31 10:02:02 +0300

    Create All Star Code Challenge.cpp

---
+++ C++/All Star Code Challenge.cpp
@@ -0,0 +1,15 @@
+/*
+ * Create a function that takes a string argument and returns that same string with all vowels removed (vowels are "a", "e", "i", "o", "u").
+ * 
+ * Example (Input --> Output)
+ * 
+ * "drake" --> "drk"
+ * "aeiou" --> ""
+ * remove_vowels("drake") // => "drk"
+ * remove_vowels("aeiou") // => ""
+ */
+#include <string>
+
+std::string remove_vowels(const std::string& str) {
+  return "";
+}

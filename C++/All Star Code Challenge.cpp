04eb61748aefa6deaf7747689098b8111fd459ca
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2023-12-31 11:04:23 +0300

    Update All Star Code Challenge.cpp

--- C++/All Star Code Challenge.cpp
+++ C++/All Star Code Challenge.cpp
@@ -13,6 +13,25 @@
 std::string remove_vowels(const std::string& str) {
   std::string output = str;
   std::string vowels = "aeiou";
-  output.erase(std::remove_if(output.begin(), output.end(), [&](char ch) { return vowels.find(ch) != std::string::npos;}));
+  output.erase(std::remove_if(output.begin(), output.end(), [&](char ch) { return vowels.find(ch) != std::string::npos;}), output.end());
   return output;
+}
+
+// ALTERNATIVE
+#include <string>
+#include <regex>
+
+std::string remove_vowels(const std::string& str) {
+  return std::regex_replace(str, std::regex("[aeiou]"), "");
+}
+
+// ALTERNATIVE 2
+std::string remove_vowels(const std::string& str) {
+  std::string result;
+  
+  for (const char ch : str)
+    if (std::string("aeiou").find(ch) == std::string::npos)
+      result.push_back(ch);
+  
+  return result;
 }

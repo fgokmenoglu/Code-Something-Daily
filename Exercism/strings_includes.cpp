0f46063376c1d23f42d9c3d1f2bdf5da2b56b4e1
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-01-06 08:01:59 +0300

    Create strings_includes.cpp

---
+++ Exercism/strings_includes.cpp
@@ -0,0 +1,23 @@
+/*
+Includes
+In general, you don't want to reinvent the wheel when it comes to programming. For many standard algorithms, there will be someone who might have already done your work for you.
+
+Include directive
+To include the content of other files C++ uses the include directive. These includes should be stated at the top of the file. During compilation, the line with the directive is replaced by the content of the respective file. Namespaces are kept as they are in the original file after includes.
+
+The Standard Library
+The standard library offers many common functions, algorithms, and data structures. The standard library uses the std namespace.
+
+One example is the C numerics library cmath. It provides many common mathematical operations.
+*/
+
+/*
+  Task 1: Implement the log_line::message method to return a log line's message.
+  Task 2: Implement the log_line::log_level method to return a log line's log level, which should be returned in uppercase.
+  Task 3: Implement the log_line::reformat method that reformats the log line, putting the message first and the log level after it in parentheses.
+*/
+#include <string>
+
+namespace log_line {
+
+} // namespace log_line

a4312113710235ec95247f1c8cd7b59838ca5ea1
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-01-06 08:03:01 +0300

    Update strings_includes.cpp

--- Exercism/strings_includes.cpp
+++ Exercism/strings_includes.cpp
@@ -1,13 +1,16 @@
 /*
-Includes
-In general, you don't want to reinvent the wheel when it comes to programming. For many standard algorithms, there will be someone who might have already done your work for you.
+INTRODUCTION
+In general, you don't want to reinvent the wheel when it comes to programming. 
+For many standard algorithms, there will be someone who might have already done your work for you.
 
-Include directive
-To include the content of other files C++ uses the include directive. These includes should be stated at the top of the file. During compilation, the line with the directive is replaced by the content of the respective file. Namespaces are kept as they are in the original file after includes.
+INCLUDE DIRECTIVE
+To include the content of other files C++ uses the include directive. 
+These includes should be stated at the top of the file. 
+During compilation, the line with the directive is replaced by the content of the respective file. 
+Namespaces are kept as they are in the original file after includes.
 
-The Standard Library
+THE STANDARD LIBRARY
 The standard library offers many common functions, algorithms, and data structures. The standard library uses the std namespace.
-
 One example is the C numerics library cmath. It provides many common mathematical operations.
 */
 

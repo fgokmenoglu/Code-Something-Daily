661915d23920c224208911bbd065c37ee7ab6c42
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-01-09 14:30:11 +0300

    Create All Star Code Challenge.cpp

---
+++ Codewars/2024/6kyu/All Star Code Challenge.cpp
@@ -0,0 +1,15 @@
+/* 
+ * Task:
+ * The scroller works by replacing the current text string with a similar text string, but with the first letter shifted to the end; this simulates movement.
+ * Your father is far too busy with the business to worry about such details, so, naturally, he's making you come up with the text strings.
+ * Create a function named rotate() that accepts a string argument and returns an array of strings with each letter from the input string being rotated to the end.
+ *
+ * Example:
+ * rotate("Hello") // => {"elloH", "lloHe", "loHel", "oHell", "Hello"}
+ *
+ * Notes:
+ * The original string should be included in the output array. 
+ * The order matters. Each element of the output array should be the rotated version of the previous element. 
+ * The output array SHOULD be the same length as the input string. 
+ * The function should return an empty array with a 0 length string, '', as input.
+ */

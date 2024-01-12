b82f26079b242cab5bd9a2b762dd0c850c4e9864
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-01-12 07:29:57 +0300

    Create Sum The Tree.cpp

---
+++ Codewars/2024/6kyu/Sum The Tree.cpp
@@ -0,0 +1,14 @@
+/* Given a node object representing a binary tree:
+ *
+ * struct node {
+ *   int value;
+ *   node* left;
+ *   node* right;
+ * }
+ *
+ * Write a function that returns the sum of all values, including the root. 
+ * Absence of a node will be indicated with a null value.
+*/
+int sumTheTreeValues(node* root) {
+  // Put the magic here
+}

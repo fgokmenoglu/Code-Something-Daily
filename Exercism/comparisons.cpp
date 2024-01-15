ba413d803b10d9b6403538caf51f87c12a76a3e8
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-01-15 07:56:14 +0300

    Create comparisons.cpp

---
+++ Exercism/comparisons.cpp
@@ -0,0 +1,26 @@
+/*
+ * INTRODUCTION
+ * In C++, you can compare values using the comparison operators: <, >, <=, >=, ==, !=
+ * The result of these comparisons is always a boolean value.
+ * This kind of comparison is available for numbers and strings. 
+ * When comparing strings, the dictionary order (also known as lexicographic order) is followed.
+ *
+ * NOTES
+ * Since C++17 if-statements can also include a short initialization statement that can be used to initialize one or more variables for the if statement.
+ * Any variables created in the initialization cannot be accessed after the end of the if statement.
+ * Example:
+ * int num{7};
+ *
+ * if (int v{2 * num}; v > 10) {
+ *   return v;
+ * } else {
+ *   return num;
+ * }
+ */
+
+/*
+ * TASK 1: Determine if you will need a driver's license
+ * TASK 2: Choose between two potential vehicles to buy
+ * TASK 3: Calculate an estimation for the price of a used vehicle
+ */
+

4982c036c9f5e095d00b540f11c4e0201bccd188
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-01-20 10:53:58 +0300

    Create Transform To Prime.cpp

---
+++ Codewars/2024/6kyu/Transform To Prime.cpp
@@ -0,0 +1,16 @@
+/*
+ * Task
+ * Given a List [] of n integers , find minimum number to be inserted in a list, so that sum of all elements of list should equal the closest prime number.
+ * 
+ * Notes
+ * List size is at least 2 .
+ * List's numbers will only positives (n > 0) .
+ * Repetition of numbers in the list could occur .
+ * The newer list's sum should equal the closest prime number .
+ *
+ * Input >> Output Examples
+ * minimumNumber ({3,1,2}) ==> return (1)
+ * Explanation:
+ * Since , the sum of the list's elements equal to (6) , the minimum number to be inserted to transform the sum to prime number is (1), 
+ * which will make the sum of the List equal the closest prime number (7) .
+ */

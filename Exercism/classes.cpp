a8fc5c08b858bbe4a73b27268f2c5095410aed29
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-01-24 08:32:45 +0300

    Update classes.cpp

--- Exercism/classes.cpp
+++ Exercism/classes.cpp
@@ -28,3 +28,53 @@
  * The class keyword is preferred for objects that need to ensure certain properties. 
  */
 
+namespace targets {
+// TODO: Insert the code for the alien class here
+    class Alien {
+        public:
+            Alien(int x, int y) {
+                x_coordinate = x;
+                y_coordinate = y;
+            }
+            
+            int get_health() {
+                return health;
+            }
+
+            bool hit() {
+                if (health > 0) {
+                    health -= 1;
+                    return true;
+                }
+
+                return false;
+            }
+
+            bool is_alive() {
+                if (health > 0)
+                    return true;
+
+                return false;
+            }
+
+            bool teleport(int x_new, int y_new) {
+                x_coordinate = x_new;
+                y_coordinate = y_new;
+                
+                return true;
+            }
+
+            bool collision_detection(Alien alien) {
+                if (x_coordinate == alien.x_coordinate && y_coordinate == alien.y_coordinate)
+                    return true;
+
+                return false;
+            }
+
+            int x_coordinate{0};
+            int y_coordinate{0};
+        private:
+            int health{3};
+    };
+
+}  // namespace targets

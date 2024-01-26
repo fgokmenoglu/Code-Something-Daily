6ad0ca3f752e1fbd162a49a45af3be8f442bcbdd
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-01-26 07:58:59 +0300

    Create Unique In Order.cpp

---
+++ Codewars/2024/6kyu/Unique In Order.cpp
@@ -0,0 +1,22 @@
+/*
+ * Implement the function unique_in_order which takes as argument a sequence 
+ * and returns a list of items without any elements 
+ * with the same value next to each other 
+ * and preserving the original order of elements.
+ *
+ * For example:
+ *
+ * uniqueInOrder("AAAABBBCCDAABBB") == {'A', 'B', 'C', 'D', 'A', 'B'}
+ * uniqueInOrder("ABBCcAD")         == {'A', 'B', 'C', 'c', 'A', 'D'}
+ * uniqueInOrder([1,2,2,3,3])       == {1,2,3}
+ */
+#include <string>
+#include <vector>
+
+template <typename T> std::vector<T> uniqueInOrder(const std::vector<T>& iterable) {
+  //your code here
+}
+
+std::vector<char> uniqueInOrder(const std::string& iterable) {
+  //your code here
+}

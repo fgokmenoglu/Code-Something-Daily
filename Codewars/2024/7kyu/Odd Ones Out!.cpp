2320793eb58a4add7d588d7b2564acdf22205914
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-01-28 20:57:59 +0300

    Create Odd Ones Out!.cpp

---
+++ Codewars/2024/7kyu/Odd Ones Out!.cpp
@@ -0,0 +1,38 @@
+/*
+ * The town sheriff dislikes odd numbers and wants all odd numbered families out of town! 
+ * In town crowds can form and individuals are often mixed with other people and families. 
+ * However you can distinguish the family they belong to by the number on the shirts they wear. 
+ * As the sheriff's assistant it's your job to find all the odd numbered families and remove them from the town!
+ * Challenge: You are given a vector of numbers. The numbers each repeat a certain number of times. 
+ * Remove all numbers that repeat an odd number of times while keeping everything else the same.
+ * oddOnesOut({1, 2, 3, 1, 3, 3}) = {1, 1}
+ * In the above example:
+ * the number 1 appears twice
+ * the number 2 appears once
+ * the number 3 appears three times
+ * 2 and 3 both appear an odd number of times, so they are removed from the list. The final result is: [1,1]
+ *
+ * Here are more examples:
+ * oddOnesOut({1, 1, 2, 2, 3, 3, 3}) = {1, 1, 2, 2}
+ * oddOnesOut({26, 23, 24, 17, 23, 24, 23, 26}) = {26, 24, 24, 26}
+ * oddOnesOut({1, 2, 3}) = {}
+ * oddOnesOut({1}) = {}
+ */
+#include <vector>
+#include <unordered_map>
+
+std::vector<int> oddOnesOut(const std::vector<int>& numbers) {
+  std::vector<int> out{numbers};
+  std::unordered_map<int, int> counts;
+
+  // Counting occurrences of each element
+  for (int element : out)
+    counts[element]++;
+
+  // Removing elements with odd counts
+  out.erase(std::remove_if(out.begin(), out.end(), 
+                             [&counts](int element) { return counts[element] % 2 != 0; }),
+              out.end());
+  
+  return out;
+}

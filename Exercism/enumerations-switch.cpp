c93bc6cfdc2c308f6eb63f1a9d5e616c88aa5069
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-01-31 13:11:06 +0300

    Update enumerations-switch.cpp

--- Exercism/enumerations-switch.cpp
+++ Exercism/enumerations-switch.cpp
@@ -31,6 +31,7 @@
  * Unscoped enumerations are becoming less popular because they all share the same global namespace. 
  * Because of the sharing, you could not have two unscoped enumerations with the same enumerators.
  * Also, unscoped enumerations implicitly convert to integers.
+ * If you want to convert scoped enumerations to integers you can use static_cast<int>.
  *
  * EXTRA
  * Switch Statements
@@ -45,3 +46,80 @@
  * The main use case for this continued execution feature is a statement that has several labels. 
  * Multiple switch results can map to the same piece of code to be executed. 
  */
+namespace hellmath {
+
+// TODO: Task 1 - Define an `AccountStatus` enumeration to represent the four
+// account types: `troll`, `guest`, `user`, and `mod`.
+enum class AccountStatus {
+    troll,
+    guest,
+    user,
+    mod
+};
+
+// TODO: Task 1 - Define an `Action` enumeration to represent the three
+// permission types: `read`, `write`, and `remove`.
+enum class Action {
+    read,
+    write,
+    remove
+};
+
+// TODO: Task 2 - Implement the `display_post` function, that gets two arguments
+// of `AccountStatus` and returns a `bool`. The first argument is the status of
+// the poster, the second one is the status of the viewer.
+bool display_post(AccountStatus posterStatus, AccountStatus viewerStatus) {
+    if (posterStatus == AccountStatus::troll && viewerStatus != AccountStatus::troll)
+        return false;
+    
+    if (posterStatus != AccountStatus::troll && viewerStatus == AccountStatus::troll)
+        return false;
+    
+    return true; // none is a troll, so all is good
+}
+
+// TODO: Task 3 - Implement the `permission_check` function, that takes an
+// `Action` as a first argument and an `AccountStatus` to check against. It
+// should return a `bool`.
+bool permission_check(Action action, AccountStatus accountStatus) {
+    switch (accountStatus) {
+        case AccountStatus::guest:
+            if (action == Action::read)
+                return true;
+            
+            return false;
+        case AccountStatus::user:
+        case AccountStatus::troll:
+            if (action == Action::read || action == Action::write)
+                return true;
+
+            return false;
+        default: // AccountStatus::mod
+            return true;
+    }
+}
+
+// TODO: Task 4 - Implement the `valid_player_combination` function that
+// checks if two players can join the same game. The function has two parameters
+// of type `AccountStatus` and returns a `bool`.
+bool valid_player_combination(AccountStatus player1Status, AccountStatus player2Status) {
+    if (player1Status == AccountStatus::guest || player2Status == AccountStatus::guest)
+        return false;
+
+    if (player1Status == AccountStatus::troll && player2Status != AccountStatus::troll)
+        return false;
+    
+    if (player1Status != AccountStatus::troll && player2Status == AccountStatus::troll)
+        return false;
+    
+    return true; // none is a guest or troll, so all is good    
+}
+
+// TODO: Task 5 - Implement the `has_priority` function that takes two
+// `AccountStatus` arguments and returns `true`, if and only if the first
+// account has a strictly higher priority than the second.
+bool has_priority (AccountStatus player1Status, AccountStatus player2Status) {
+    return static_cast<int>(player1Status) > static_cast<int>(player2Status);
+}
+
+}  // namespace hellmath

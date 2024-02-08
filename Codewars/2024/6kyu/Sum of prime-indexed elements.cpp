b203939990127eb1dbab4b6a215ba8bb8aae7a99
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-02-08 08:06:14 +0300

    Update Sum of prime-indexed elements.cpp

--- Codewars/2024/6kyu/Sum of prime-indexed elements.cpp
+++ Codewars/2024/6kyu/Sum of prime-indexed elements.cpp
@@ -1,6 +1,9 @@
 /*
- * In this Kata, you will be given an integer array and your task is to return the sum of elements occupying prime-numbered indices.
- * The first element of the array is at index 0.
+ * In this Kata, you will be given an integer array ... 
+ * and your task is to return the sum of elements ...
+ * occupying prime-numbered indices.
+ *
+ * Note: The first element of the array is at index 0.
  */
 #include <vector>
 

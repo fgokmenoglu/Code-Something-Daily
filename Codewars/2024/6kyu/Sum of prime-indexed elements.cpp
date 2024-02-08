bff7e57e8296b97d4bd7d5450a92e8d33401be55
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-02-08 07:21:24 +0300

    Create Sum of prime-indexed elements.cpp

---
+++ Codewars/2024/6kyu/Sum of prime-indexed elements.cpp
@@ -0,0 +1,9 @@
+/*
+ * In this Kata, you will be given an integer array and your task is to return the sum of elements occupying prime-numbered indices.
+ * The first element of the array is at index 0.
+ */
+#include <vector>
+
+int solve(std::vector<int> v) {
+  //..
+}

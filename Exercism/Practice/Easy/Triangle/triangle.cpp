3d1d988f4ea67982ff10458bbe4605816102fc33
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-02-13 07:51:45 +0300

    Update triangle.cpp

--- Exercism/Practice/Easy/Triangle/triangle.cpp
+++ Exercism/Practice/Easy/Triangle/triangle.cpp
@@ -1,1 +1,18 @@
+#include "triangle.h"
+#include <stdexcept>
 
+namespace triangle {
+
+    flavor kind(double a, double b, double c) {       
+        if (a <= 0 || b <= 0 || c <= 0 || (a + b) <= c || (a + c) <= b || (b + c) <= a)
+            throw std::domain_error("");
+
+        if (a == b && b == c)
+            return flavor::equilateral;
+        else if (a == b || b == c || a == c)
+            return flavor::isosceles;
+        else
+            return flavor::scalene;
+    }
+
+}  // namespace triangle

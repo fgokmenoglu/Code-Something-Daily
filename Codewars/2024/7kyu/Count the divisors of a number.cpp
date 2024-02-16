89420b2eebc3f2deecb5df7455aa82a2e4bea65f
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-02-16 08:23:13 +0300

    Update Count the divisors of a number.cpp

--- Codewars/2024/7kyu/Count the divisors of a number.cpp
+++ Codewars/2024/7kyu/Count the divisors of a number.cpp
@@ -20,3 +20,11 @@ int divisors(int n) {
       
   return divCnt;
 }
+
+/*
+ * There is a number after n / 2 which is a divisor: The number itself. 
+ * Self-division is accounted for by instantiating the count at 1, however.
+ * To qualify as a divisor, dividing by anything larger than half the original number (ie: n / 2), 
+ * but smaller than the number itself (ie: n), would need to result in an integer somewhere between 1 and 2. 
+ * No such number exists, by definition. So we can confidently exclude those considerations.
+ */

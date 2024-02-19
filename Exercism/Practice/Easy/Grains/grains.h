55d46af6557e207b0b5094c81e3f79d8c71d7a0b
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-02-19 07:39:36 +0300

    Update grains.h

--- Exercism/Practice/Easy/Grains/grains.h
+++ Exercism/Practice/Easy/Grains/grains.h
@@ -1,8 +1,11 @@
 #if !defined(GRAINS_H)
 #define GRAINS_H
 
+#include <cmath>
+
 namespace grains {
     unsigned long long square(int n);
+    unsigned long long total();
 }  // namespace grains
 
 #endif // GRAINS_H

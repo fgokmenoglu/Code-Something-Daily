151271818e84f046b113c87555daaec40c67370e
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-02-21 07:45:46 +0300

    Create pangram.h

---
+++ Exercism/Practice/Easy/Pangram/pangram.h
@@ -0,0 +1,11 @@
+#if !defined(PANGRAM_H)
+#define PANGRAM_H
+
+#include <string>
+#include <algorithm>
+
+namespace pangram {
+    bool is_pangram(std::string in);
+}  // namespace pangram
+
+#endif // PANGRAM_H

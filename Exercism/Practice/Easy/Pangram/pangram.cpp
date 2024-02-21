37a670a266e4844e83e89ef207dd78d9e1172e03
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-02-21 07:51:03 +0300

    Update pangram.cpp

--- Exercism/Practice/Easy/Pangram/pangram.cpp
+++ Exercism/Practice/Easy/Pangram/pangram.cpp
@@ -21,3 +21,24 @@ namespace pangram {
         return out;
     }
 }  // namespace pangram
+
+// ALTERNATIVE
+#include "pangram.h"
+#include <bitset>
+
+namespace pangram {
+	bool is_pangram(const std::string sentence) {
+		std::bitset<26> alphabet_flags;
+
+		for (const auto c : sentence) {
+			if (isalpha(c)) {
+				alphabet_flags.set(tolower(c) - 'a');
+
+				if (alphabet_flags.all())
+					return true;
+			}
+		}
+
+		return false;
+	}
+}

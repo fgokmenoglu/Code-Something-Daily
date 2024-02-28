13aed281f7bd7488e14bab07c474e4637b242aa5
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-02-28 10:02:50 +0300

    Update Parts of a list.cpp

--- Codewars/2024/7kyu/Parts of a list.cpp
+++ Codewars/2024/7kyu/Parts of a list.cpp
@@ -44,3 +44,28 @@ public:
     return out;
   }
 };
+
+// ALTERNATIVE
+#include <vector>
+#include <utility>
+#include <string>
+#include <numeric>
+
+using namespace std;
+
+class PartList
+{
+public:
+  static std::vector<std::pair <std::string, std::string>> partlist(std::vector<std::string> &arr) {
+    vector<pair<string, string>> result;
+    auto combine = [](string a, string b) { return a + ' ' + b; };
+    
+    for (int i = 1; i < arr.size(); i++) {
+      result.emplace_back(
+        accumulate(arr.begin() + 1, arr.begin() + i, arr[0], combine),
+        accumulate(arr.begin() + i + 1,  arr.end(), arr[i], combine));
+    }
+      
+    return result;
+  }
+};

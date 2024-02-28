1e515918d2eef88f54c033b8eb2a6a6d712f43d2
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-02-28 10:04:31 +0300

    Update Parts of a list.cpp

--- Codewars/2024/7kyu/Parts of a list.cpp
+++ Codewars/2024/7kyu/Parts of a list.cpp
@@ -4,21 +4,10 @@
  * Each two non empty parts will be in a pair (or an array for languages without tuples or a structin C - C: see Examples test Cases - )
  * Each part will be in a string
  * Elements of a pair must be in the same order as in the original array.
- * Examples of returns in different languages:
- * a = ["az", "toto", "picaro", "zone", "kiwi"] -->
- * [["az", "toto picaro zone kiwi"], ["az toto", "picaro zone kiwi"], ["az toto picaro", "zone kiwi"], ["az toto picaro zone", "kiwi"]] 
- * or
- *  a = {"az", "toto", "picaro", "zone", "kiwi"} -->
+ *
+ * Example:
+ * a = {"az", "toto", "picaro", "zone", "kiwi"} -->
  * {{"az", "toto picaro zone kiwi"}, {"az toto", "picaro zone kiwi"}, {"az toto picaro", "zone kiwi"}, {"az toto picaro zone", "kiwi"}}
- * or
- * a = ["az", "toto", "picaro", "zone", "kiwi"] -->
- * [("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
- * or 
- * a = [|"az", "toto", "picaro", "zone", "kiwi"|] -->
- * [("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
- * or
- * a = ["az", "toto", "picaro", "zone", "kiwi"] -->
- * "(az, toto picaro zone kiwi)(az toto, picaro zone kiwi)(az toto picaro, zone kiwi)(az toto picaro zone, kiwi)"
  */
 #include <vector>
 #include <utility>

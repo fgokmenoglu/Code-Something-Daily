45088406ba4cca608f22718ccd49ba47c1eecd4f
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-03-02 13:06:26 +0300

    Update Abbreviate a Two Word Name.cpp

--- Codewars/2024/8kyu/Abbreviate a Two Word Name.cpp
+++ Codewars/2024/8kyu/Abbreviate a Two Word Name.cpp
@@ -7,3 +7,31 @@
  * Sam Harris => S.H
  * patrick feeney => P.F
  */
+#include <string>
+#include <cctype>
+
+std::string abbrevName(std::string name) {
+  std::string first_name = name.substr(0, name.find(' '));
+  std::string last_name = name.substr(name.find(' ') + 1);
+  std::string out = "";
+  
+  out.push_back(std::toupper(first_name[0]));
+  out.push_back('.');
+  out.push_back(std::toupper(last_name[0]));
+  
+  return out;
+}
+
+// ALTERNATIVE
+#include <string>
+#include <cctype>
+
+std::string abbrevName(std::string name) {
+  std::string ret;
+  
+  ret.push_back(toupper(name[0]));
+  ret.push_back('.');
+  ret.push_back(toupper(name[name.find(' ') + 1] ));
+  
+  return ret;
+}

aa33ea126041f73d216b7dd98a084c0e3c597042
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-03-03 09:32:45 +0300

    Update A wolf in sheep's clothing.cpp

--- Codewars/2024/8kyu/A wolf in sheep's clothing.cpp
+++ Codewars/2024/8kyu/A wolf in sheep's clothing.cpp
@@ -26,5 +26,10 @@
 #include <string>
 
 std::string warn_the_sheep(const std::vector<std::string>& queue) {
-    return "";
+  for (int i = queue.size() - 1; i >= 0; --i)
+    if (queue[i] == "wolf")
+      if (i == queue.size() - 1) 
+        return "Pls go away and stop eating my sheep";
+      else 
+        return "Oi! Sheep number " + std::to_string(queue.size() - i - 1) + "! You are about to be eaten by a wolf!";
 }

5e10fb36fe527e8ce18892fa09da144c9be3605f
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-03-04 07:40:25 +0300

    Update Count the Monkeys!.cpp

--- Codewars/2024/8kyu/Count the Monkeys!.cpp
+++ Codewars/2024/8kyu/Count the Monkeys!.cpp
@@ -12,3 +12,24 @@
  * 10 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
  * 1 --> [1]
  */
+#include <vector>
+
+std::vector<int> MonkeyCount(int n) {
+  std::vector<int> out(n);
+  
+  for (int i = 1; i <= n; ++i)
+    out[i - 1] = i;
+  
+  return out;
+}
+
+// ALTERNATIVE
+#include <vector>
+#include <numeric>
+
+std::vector<int> MonkeyCount(int n) {
+  std::vector<int> res(n);
+  std::iota(res.begin(), res.end(), 1);
+  
+  return res;
+}

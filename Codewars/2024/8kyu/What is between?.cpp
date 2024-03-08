d958a4e144646485b54fae578c2c063da5de9a10
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-03-08 13:44:26 +0300

    Create What is between?.cpp

---
+++ Codewars/2024/8kyu/What is between?.cpp
@@ -0,0 +1,29 @@
+/* Complete the function that takes two integers (a, b, where a < b) and 
+ * return an array of all integers between the input parameters, including them.
+ *
+ * For example:
+ * a = 1
+ * b = 4
+ * --> [1, 2, 3, 4]
+ */
+#include <vector>
+
+std::vector<int> between(int start, int end) {
+  std::vector<int> out(end - start + 1);
+  
+  for (int i = start, j = 0; i <= end; ++i)
+    out[j++] = i;
+  
+  return out;
+}  
+
+// ALTERNATIVE
+#include <vector>
+#include <numeric>
+
+std::vector<int> between(int start, int end) {
+  std::vector<int> vec(end - start + 1);
+  std::iota(vec.begin(), vec.end(), start);
+  
+  return vec;
+}  

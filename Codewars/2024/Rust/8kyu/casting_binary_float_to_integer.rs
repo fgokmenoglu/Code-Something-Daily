7a4270201cd23078dc48cf74a1cd944335359485
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-03-16 11:42:56 +0300

    Create casting_binary_float_to_integer.rs

---
+++ Codewars/2024/Rust/8kyu/casting_binary_float_to_integer.rs
@@ -0,0 +1,13 @@
+/*
+ * You are given a float (single precision) which means, 
+ * that 32 bit are used to hold a value.
+ *
+ * Return the number an integer would hold with the same bit arrangement
+ *
+ * 10.0 (f32) == 01000001001000000000000000000000 (binary)
+ * convert_to_i32(10.0) returns 1092616192 (i32)
+ */
+// return binary representation as i32
+pub fn convert_to_i32(f: f32) -> i32 {
+    // TODO
+}

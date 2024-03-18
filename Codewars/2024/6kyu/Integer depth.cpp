e55b7cfa34d84c71d9d6c4806a20bd50ece07711
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-03-18 08:13:18 +0300

    Update Integer depth.cpp

--- Codewars/2024/6kyu/Integer depth.cpp
+++ Codewars/2024/6kyu/Integer depth.cpp
@@ -20,6 +20,57 @@
  * Write a function named computeDepth which computes the depth of its integer argument.
  * Only positive numbers greater than zero will be passed as an input.
  */
+#include <map>
+
 unsigned integer_depth(unsigned n) {
-  return 0;
+  unsigned out = 1;
+  unsigned temp = n;
+  std::map<unsigned, unsigned> digits = {{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}, {6, 0}, {7, 0}, {8, 0}, {9, 0}};
+  
+  while (true) {    
+    while (temp > 0) {
+      digits[temp % 10] += 1;
+      temp /= 10;
+    }
+    
+    bool isDone = true;
+    
+    for (unsigned i = 0; i < 10; ++i)
+      if (digits[i] == 0)
+        isDone = false;
+    
+    if (isDone)
+      break;
+    
+    out += 1;
+    temp = n * out;
+  }
+  
+  return out;
+}
+
+// ALTERNATIVE
+unsigned integer_depth(unsigned n) {
+    bool seen[10] = {0};  // Array to keep track of seen digits
+    unsigned count = 0;   // Count of multiplications
+    unsigned total_seen = 0;  // Total number of unique digits seen
+
+    while (total_seen < 10) {
+        count++;
+        unsigned product = n * count;
+
+        // Process each digit of the product
+        while (product > 0) {
+            unsigned digit = product % 10;
+          
+            if (!seen[digit]) {
+                seen[digit] = true;
+                total_seen++;
+            }
+          
+            product /= 10;
+        }
+    }
+
+    return count;
 }

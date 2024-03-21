653f4d9145797676d1f4e8db781dd32b8ceabf92
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-03-21 15:01:21 +0300

    Update last.rs

--- Codewars/2024/Rust/7kyu/last.rs
+++ Codewars/2024/Rust/7kyu/last.rs
@@ -5,10 +5,6 @@
  * last(&[1, 2, 3, 4])     // =>  4
  * last(&['x', 'y', 'z'])  // => 'z'
  */
-fn last<T: Clone>(slice: &[T]) -> Option<T> {
-    if slice.is_empty() {
-        None
-    } else {
-        Some(slice.last().unwrap().clone())
-    }
+fn last<T: Clone>(slice: &[T]) -> T {
+    slice.last().unwrap().clone()
 }

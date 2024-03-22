ab1c2708be932e1cb205deb0e6b96064239882c6
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-03-22 10:55:57 +0300

    Create alphabetically_ordered.rs

---
+++ Codewars/2024/Rust/7kyu/alphabetically_ordered.rs
@@ -0,0 +1,11 @@
+/*
+ * Write a function that takes an input string of lowercase letters 
+ * and returns true/false depending on whether the string is in alphabetical order or not.
+ *
+ * Examples (input -> output)
+ * "kata" -> false ('a' comes after 'k')
+ * "ant" -> true (all characters are in alphabetical order)
+ */
+fn alphabetic(s: &str) -> bool {
+    todo!()
+}

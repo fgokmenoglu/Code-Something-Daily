7e5509615d6c6780ccf58efa16aaa36dc4266239
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-03-25 19:58:27 +0300

    Update and rename Return the first M multiples of N.rs to return_first_M_multiples_of_N.rs

--- Codewars/2024/Rust/7kyu/return_first_M_multiples_of_N.rs
+++ Codewars/2024/Rust/7kyu/return_first_M_multiples_of_N.rs
@@ -4,3 +4,12 @@
  * multiples of the real number n. 
  * Assume that m is a positive integer.
  */
+fn multiples(m: i32, n: f64) -> Vec<f64> {
+    let mut result = Vec::new();
+    
+    for i in 1..=m {
+        result.push(n * i as f64);
+    }
+    
+    result
+}

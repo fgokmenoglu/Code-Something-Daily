81d465af164ae7aa31d7f6848348cf5ef02248bb
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-04-07 14:38:56 +0300

    Update find_multiples_of_number.rs

--- Codewars/2024/Rust/8kyu/find_multiples_of_number.rs
+++ Codewars/2024/Rust/8kyu/find_multiples_of_number.rs
@@ -18,3 +18,10 @@ fn find_multiples(n: u32, limit: u32) -> Vec<u32> {
 
     multiples
 }
+
+// ALTERNATIVE
+fn find_multiples(n: u32, limit: u32) -> Vec<u32> {
+    (n..=limit)
+        .step_by((n) as usize)
+        .collect()
+}

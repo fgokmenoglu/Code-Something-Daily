6f77273688d9e59b62a2c8fd23368dd589accdc7
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-04-09 11:27:10 +0300

    Create building_blocks.rs

---
+++ Codewars/2024/Rust/7kyu/building_blocks.rs
@@ -0,0 +1,30 @@
+/*
+ * Write a class Block that creates a block.
+ *
+ * Requirements
+ * The constructor should take an array as an argument, 
+ * this will contain 3 integers of the form [width, length, height] 
+ * from which the Block should be created.
+ *
+ * Define these methods:
+ * new -> initialize the `Block` from the provided array of u32
+ * // all the methods must return a u32
+ * get_width() -> width of the `Block`
+ * get_length() -> length of the `Block`
+ * get_height() -> height of the `Block`
+ * get_volume() -> volume of the `Block`
+ * get_surface_area() -> surface area of the `Block`
+ *
+ * Examples
+ * let b = Block::new(&[2,4,6]) -> create a `Block` object with a width of `2` a length of `4` and a height of `6`
+ * b.get_width() -> return 2
+ * b.get_length() -> return 4
+ * b.get_height() -> return 6
+ * b.get_volume() -> return 48
+ * b.get_surface_area() -> return 88
+ * 
+ * Note: no error checking is needed
+ */
+struct Block {
+    // ...
+}

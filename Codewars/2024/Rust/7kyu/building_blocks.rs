2c608a131d465f71e287d983cf788c47240b383d
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-04-09 11:29:28 +0300

    Update building_blocks.rs

--- Codewars/2024/Rust/7kyu/building_blocks.rs
+++ Codewars/2024/Rust/7kyu/building_blocks.rs
@@ -26,5 +26,43 @@
  * Note: no error checking is needed
  */
 struct Block {
-    // ...
+    width: u32,
+    length: u32,
+    height: u32,
+}
+
+impl Block {
+    // Initialize the `Block` from the provided array of u32
+    fn new(dimensions: &[u32; 3]) -> Block {
+        Block {
+            width: dimensions[0],
+            length: dimensions[1],
+            height: dimensions[2],
+        }
+    }
+
+    // Method to get the width of the `Block`
+    fn get_width(&self) -> u32 {
+        self.width
+    }
+
+    // Method to get the length of the `Block`
+    fn get_length(&self) -> u32 {
+        self.length
+    }
+
+    // Method to get the height of the `Block`
+    fn get_height(&self) -> u32 {
+        self.height
+    }
+
+    // Method to calculate the volume of the `Block`
+    fn get_volume(&self) -> u32 {
+        self.width * self.length * self.height
+    }
+
+    // Method to calculate the surface area of the `Block`
+    fn get_surface_area(&self) -> u32 {
+        2 * (self.width * self.length + self.width * self.height + self.length * self.height)
+    }
 }

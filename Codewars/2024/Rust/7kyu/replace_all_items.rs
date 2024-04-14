9fce81afb0574387c3ff4f07366b66f7f8029a10
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-04-14 16:49:05 +0300

    Update replace_all_items.rs

--- Codewars/2024/Rust/7kyu/replace_all_items.rs
+++ Codewars/2024/Rust/7kyu/replace_all_items.rs
@@ -1,7 +1,10 @@
 /*
-Write function replaceAll that will replace all occurrences of an item with another.
-
-Example: replaceAll [1,2,2] 1 2 -> in list [1,2,2] we replace 1 with 2 to get new list [2,2,2]
-
-
+ * Write function replaceAll that will replace all occurrences of an item with another.
+ *
+ * Example: replaceAll [1,2,2] 1 2 -> in list [1,2,2] we replace 1 with 2 to get new list [2,2,2]
  */
+fn replace_all<T: PartialEq + Copy>(xs: &[T], find: T, replace: T) -> Vec<T> {
+    xs.iter()
+      .map(|&x| if x == find { replace } else { x })
+      .collect()
+}

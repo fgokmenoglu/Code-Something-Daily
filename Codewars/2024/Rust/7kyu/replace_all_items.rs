4f902c32b8749212b2fb97d0dad64f2d79f60a42
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-04-14 15:32:24 +0300

    Create replace_all_items.rs

---
+++ Codewars/2024/Rust/7kyu/replace_all_items.rs
@@ -0,0 +1,7 @@
+/*
+Write function replaceAll that will replace all occurrences of an item with another.
+
+Example: replaceAll [1,2,2] 1 2 -> in list [1,2,2] we replace 1 with 2 to get new list [2,2,2]
+
+
+ */

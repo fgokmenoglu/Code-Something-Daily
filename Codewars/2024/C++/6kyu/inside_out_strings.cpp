65d04923a056894b0a488f6f2ba500a7c8df6ab2
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-04-27 14:32:48 +0300

    Update inside_out_strings.cpp

--- Codewars/2024/C++/6kyu/inside_out_strings.cpp
+++ Codewars/2024/C++/6kyu/inside_out_strings.cpp
@@ -10,3 +10,40 @@
  * An example should clarify:
  *'taxi' would become 'atix' 'taxis' would become 'atxsi'
  */
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+std::string insideOut(const std::string& x) {
+    std::istringstream iss(x);
+    std::vector<std::string> words;
+    std::string word;
+    
+    // Split the input string into words
+    while (iss >> word) {
+        words.push_back(word);
+    }
+    
+    // Transform each word
+    for (std::string& w : words) {
+        int len = w.size();
+        if (len % 2 == 0) {
+            // Even length: reverse both halves
+            std::reverse(w.begin(), w.begin() + len / 2);
+            std::reverse(w.begin() + len / 2, w.end());
+        } else {
+            // Odd length: reverse around the middle character
+            std::reverse(w.begin(), w.begin() + len / 2);
+            std::reverse(w.begin() + len / 2 + 1, w.end());
+        }
+    }
+    
+    // Reassemble the words back into a single string
+    std::string result;
+    for (size_t i = 0; i < words.size(); ++i) {
+        if (i > 0) result += " ";  // Add space between words
+        result += words[i];
+    }
+    
+    return result;
+}

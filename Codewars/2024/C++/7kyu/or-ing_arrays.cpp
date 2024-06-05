f556188b1853ed786d75255c349a311302bf8474
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-06-05 09:06:48 +0300

    Update or-ing_arrays.cpp

--- Codewars/2024/C++/7kyu/or-ing_arrays.cpp
+++ Codewars/2024/C++/7kyu/or-ing_arrays.cpp
@@ -15,7 +15,21 @@
  * orArrays([1,2,3],[1,2,3],3) == [1,2,3]
  */
 #include <vector>
+#include <algorithm>  // for std::max
 
-std::vector<int> orArrays(const std::vector<int>& arr1, const std::vector<int>& arr2) {
-  //your code here
+std::vector<int> orArrays(const std::vector<int>& arr1, const std::vector<int>& arr2, int defaultValue = 0) {
+    // Determine the length of the longer array
+    size_t maxLength = std::max(arr1.size(), arr2.size());
+    
+    // Create a result vector of the same length
+    std::vector<int> result(maxLength);
+    
+    // Perform the binary OR operation
+    for (size_t i = 0; i < maxLength; ++i) {
+        int val1 = (i < arr1.size()) ? arr1[i] : defaultValue;
+        int val2 = (i < arr2.size()) ? arr2[i] : defaultValue;
+        result[i] = val1 | val2;
+    }
+    
+    return result;
 }

1b4945e39a444fdd939f4e2c9f8e884617c2388c
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-06-06 07:34:05 +0300

    Create changing_letters.c

---
+++ Codewars/2024/C/7kyu/changing_letters.c
@@ -0,0 +1,25 @@
+/*
+ * When provided with a String, capitalize all vowels
+ *
+ * For example:
+ * Input : "Hello World!"
+ * Output : "HEllO WOrld!"
+ *
+ * Note: Y is not a vowel in this kata.
+ */
+#include <stdio.h>
+#include <ctype.h>
+
+char* capitalize_vowels(char* string) {
+    if (string == NULL) return NULL;
+
+    // Process the string and capitalize vowels
+    for (size_t i = 0; string[i] != '\0'; i++) {
+        char c = string[i];
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            string[i] = toupper(c);
+        }
+    }
+    
+    return string;
+}

d415b527c31b24bd149542e90b33178d094188c7
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-06-15 08:17:49 +0300

    Create evens_times_last.cpp

---
+++ Codewars/2024/C++/7kyu/evens_times_last.cpp
@@ -0,0 +1,13 @@
+/*
+ * Given a sequence of integers, 
+ * return the sum of all the integers that have an even index, 
+ * multiplied by the integer at the last index.
+ *
+ * Indices in sequence start from 0.
+ * If the sequence is empty, you should return 0.
+ */
+#include <vector>
+
+int even_last(std::vector<int> nums) {
+  return 0;
+}

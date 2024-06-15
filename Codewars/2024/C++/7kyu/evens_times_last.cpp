95d4604aabf17d50e3a4802d7aa93267e1044cac
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-06-15 08:19:54 +0300

    Update evens_times_last.cpp

--- Codewars/2024/C++/7kyu/evens_times_last.cpp
+++ Codewars/2024/C++/7kyu/evens_times_last.cpp
@@ -8,6 +8,24 @@
  */
 #include <vector>
 
-int even_last(std::vector<int> nums) {
-  return 0;
+int even_last(const std::vector<int>& nums) {
+    if (nums.empty()) {
+        return 0;
+    }
+    
+    int sum = 0;
+    for (size_t i = 0; i < nums.size(); i += 2) {
+        sum += nums[i];
+    }
+    
+    return sum * nums.back();
+}
+
+// ALTERNATIVE
+#include <vector>
+
+int even_last(const std::vector<int>& nums) {
+    int sum = 0;
+    for(auto it = nums.cbegin(); it < nums.cend(); it += 2) sum += *it;
+    return nums.empty() ? 0 : nums.back() * sum;
 }

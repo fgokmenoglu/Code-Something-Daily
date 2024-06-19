660804d186ae38086f75dc4951e48b706742ab0d
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-06-19 09:41:41 +0300

    Update distance_between_points_2D.cpp

--- Codewars/2024/Rust/7kyu/distance_between_points_2D.cpp
+++ Codewars/2024/Rust/7kyu/distance_between_points_2D.cpp
@@ -1,5 +1,9 @@
 /*
-
+ * This series of katas will introduce you 
+ * to basics of doing geometry with computers.
+ * Point objects have attributes x and y.
+ * Write a function calculating distance 
+ * between Point a and Point b.
  */
 
 #include <cmath> // For std::sqrt and std::pow

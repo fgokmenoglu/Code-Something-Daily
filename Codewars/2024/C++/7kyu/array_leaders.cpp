694d21331751596fbe64a8326e72db8c47615b3b
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-06-22 11:57:29 +0300

    Update array_leaders.cpp

--- Codewars/2024/C++/7kyu/array_leaders.cpp
+++ Codewars/2024/C++/7kyu/array_leaders.cpp
@@ -24,5 +24,5 @@
  * Explanation:
  * 17 is greater than the sum all the elements to its right side
  * 5 is greater than the sum all the elements to its right side
- * Note : The last element  2 is greater than the sum of its right elements (abstract 
+ * Note : The last element 2 is greater than the sum of its right elements (abstract zero). 
  */

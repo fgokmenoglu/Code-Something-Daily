6bf7c463163034ae3a02d53c9e2415d8e36452f7
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-07-01 06:42:03 +0300

    Update flick_switch.cpp

--- Codewars/2024/C++/8kyu/flick_switch.cpp
+++ Codewars/2024/C++/8kyu/flick_switch.cpp
@@ -1,19 +1,32 @@
 /*
-Task
-
-Create a function that always returns True/true for every item in a given list.
-However, if an element is the word 'flick', switch to always returning the opposite boolean value.
-
-Examples
-
-['codewars', 'flick', 'code', 'wars'] ➞ [True, False, False, False]
-
-['flick', 'chocolate', 'adventure', 'sunshine'] ➞ [False, False, False, False]
-
-['bicycle', 'jarmony', 'flick', 'sheep', 'flick'] ➞ [True, True, False, False, True]
-Notes
-
-"flick" will always be given in lowercase.
-A list may contain multiple flicks.
-Switch the boolean value on the same element as the flick itself.
+ * Create a function that always returns True/true 
+ * for every item in a given list. However, 
+ * if an element is the word 'flick', 
+ * switch to always returning the opposite boolean value.
+ *
+ * Examples
+ * ['codewars', 'flick', 'code', 'wars'] ➞ [True, False, False, False]
+ * ['flick', 'chocolate', 'adventure', 'sunshine'] ➞ [False, False, False, False]
+ * ['bicycle', 'jarmony', 'flick', 'sheep', 'flick'] ➞ [True, True, False, False, True]
+ *
+ * Notes
+ * "flick" will always be given in lowercase.
+ * A list may contain multiple flicks.
+ * Switch the boolean value on the same element as the flick itself.
  */
+#include <vector>
+#include <string>
+
+std::vector<bool> flick_switch(const std::vector<std::string>& arr) {
+    std::vector<bool> result;
+    bool current_state = true;
+
+    for (const auto& element : arr) {
+        if (element == "flick") {
+            current_state = !current_state;
+        }
+        result.push_back(current_state);
+    }
+
+    return result;
+}

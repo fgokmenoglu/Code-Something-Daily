4a5a84233e4f3d93a324f8f6511b64a836111abd
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-07-24 07:20:22 +0300

    Create string_contamination.rs

---
+++ Codewars/2024/Rust/8kyu/string_contamination.rs
@@ -0,0 +1,23 @@
+fn contamination(text: &str, character: &str) -> String {
+    // Task Description:
+    // An AI has infected a text with a character!!
+    // 
+    // This text is now fully mutated to this character.
+    // 
+    // If the text or the character are empty, return an empty string.
+    // There will never be a case when both are empty as nothing is going on!!
+    // 
+    // Note: The character is a string of length 1 or an empty string.
+    // 
+    // Example
+    // 
+    // text before = "abc"
+    // character   = "z"
+    // text after  = "zzz"
+
+    if text.is_empty() || character.is_empty() {
+        return String::new();
+    }
+
+    character.repeat(text.len())
+}

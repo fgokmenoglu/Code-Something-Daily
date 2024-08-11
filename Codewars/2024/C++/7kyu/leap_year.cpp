a12adc6aecf90da21f1238150beeec6ae7a0d7d8
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-08-11 08:48:19 +0300

    Create leap_year.cpp

---
+++ Codewars/2024/C++/7kyu/leap_year.cpp
@@ -0,0 +1,15 @@
+/* Task Description:
+ * In this kata you should simply determine, whether a given year is a leap year or not. In case you don't know the rules, here they are:
+ * 
+ * Years divisible by 4 are leap years,
+ * but years divisible by 100 are not leap years,
+ * but years divisible by 400 are leap years.
+ * 
+ * Tested years are in range 1600 ≤ year ≤ 4000.
+ */
+
+bool IsLeapYear(int year) {
+    if (year % 400 == 0) return true;
+    if (year % 100 == 0) return false;
+    return year % 4 == 0;
+}

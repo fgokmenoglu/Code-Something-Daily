d5b1fffb1f6123498aa7d93455f0ab03b384f049
Fatih Gökmenoğlu <fatih.gokmenoglu@gmail.com>
2024-09-10 00:32:24 +0300

    Update string_camel_case.cpp

--- Codewars/2024/C++/6kyu/string_camel_case.cpp
+++ Codewars/2024/C++/6kyu/string_camel_case.cpp
@@ -1,10 +1,10 @@
 #include <string>
 #include <cctype>
 
-// * Task: Complete the method/function so that it converts dash/underscore delimited words into camel casing.
-// * The first word within the output should be capitalized only if the original word was capitalized
-// * (known as Upper Camel Case, also often referred to as Pascal case).
-// * The next words should be always capitalized.
+// Complete the method/function so that it converts dash/underscore delimited words into camel casing.
+// The first word within the output should be capitalized only if the original word was capitalized
+// (known as Upper Camel Case, also often referred to as Pascal case).
+// The next words should be always capitalized.
 
 std::string to_camel_case(std::string text) {
     if (text.empty()) {
